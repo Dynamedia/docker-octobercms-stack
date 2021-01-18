9f7ac16d09f61d7d00d90182a7ed373073338d4f
Rob Ballantyne <rob@dynamedia.uk>
2021-01-18 16:01:42 +0000

    ready for v 1.1

--- setupdata.sh
+++ setupdata.sh
@@ -3,11 +3,12 @@
 # We want to persist some of our data on the host so create some directories. This is not strictly necessary but I want the structure to be clear
 
 # Create data directories for the October CMS app
-mkdir -p ./data/app/{storage/{media,uploads/public},plugins,themes}
-touch ./data/app/storage/media/.gitkeep
-touch ./data/app/storage/uploads/public/.gitkeep
-touch ./data/app/plugins/.gitkeep
-touch ./data/app/themes/.gitkeep
+mkdir -p ./data/{storage/{app/{media,uploads/public},logs},plugins,themes}
+touch ./data/storage/app/media/.gitkeep
+touch ./data/storage/app/uploads/public/.gitkeep
+touch ./data/storage/logs/.gitkeep
+touch ./data/plugins/.gitkeep
+touch ./data/themes/.gitkeep
 
 # Create directories for our database and cache
 mkdir ./data/{mysql,redis}
